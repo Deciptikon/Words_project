65dc0b322eddf628003762e2093f395715faa30e
Deciptikon <deciptikon@mail.ru>
2024-06-08 02:17:50 +0300

    test bttn

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
-// Находим кнопку по её id
-var button = document.getElementById("button-to-page1");
-console.log(button);
-// Добавляем обработчик события клика на кнопку
-button.addEventListener("click", () => {
-  // Получаем ссылку из атрибута data-href
-  var link = button.getAttribute("data-href");
-  // Переходим по указанной ссылке
-  console.log(link);
-  window.location.href = link;
+document.addEventListener("DOMContentLoaded", () => {
+  // Находим кнопку по её id
+  var button = document.getElementById("button-to-page1");
+  console.log("HELLO");
+  // Добавляем обработчик события клика на кнопку
+  button.addEventListener("click", () => {
+    // Получаем ссылку из атрибута data-href
+    var link = button.getAttribute("data-href");
+    // Переходим по указанной ссылке
+    console.log(link);
+    window.location.href = link;
+  });
 });
